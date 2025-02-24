04499fd302429dd683f7850713d96f7c1d353448
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-02-24 15:09:33 -0300

    Chamando a API com simulador

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -2,7 +2,18 @@ import {IncomingMessage,request,ServerResponse} from "http";
 
 export const getListEpisodes = async (req : IncomingMessage, res: ServerResponse) => {
     res.writeHead(200,{'content-type': "application/json"});
-    res.end({
-        name: "felipe",
-    });
+    res.end(JSON.stringify([
+        {
+          podcastName: "flow",
+          episode: "CBUM - Flow #319",
+          videoId:"pQSuQmUfS30",
+          categories: ["saúde", "esporte","bodybuilder"]
+        },
+        {
+          podcastName: "flow",
+          episode: "RUBENS BARRICHELLO - Flow #339",
+          videoId:"4KDGTdiOV4I",
+          categories: ["esporte", "corrida"]
+        },
+      ]));
 };
