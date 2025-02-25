d9e6a7a7eb101bfe65c57d71fc7ea06cfdfb64ca
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-02-25 10:55:11 -0300

    Refatorando Rotas

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as http from "http";
 import {getFilterEpisodes, getListEpisodes} from './controllers/podscasts-controller'
+import { Routes } from "./routes/routes";
 
 const server = http.createServer(
     async (req: http.IncomingMessage, res: http.ServerResponse) => {
@@ -11,11 +12,11 @@ const server = http.createServer(
       console.log(queryString);
 
       // Listar podcasts
-      if(req.method === "GET" && baseUrl === "/api/list") {
+      if(req.method === "GET" && baseUrl === Routes.LIST) {
         await getListEpisodes(req,res);
       }
 
-      if(req.method === "GET" && baseUrl === "/api/episode"){
+      if(req.method === "GET" && baseUrl === Routes.EPISODE){
         await getFilterEpisodes(req,res);
       }
 }
