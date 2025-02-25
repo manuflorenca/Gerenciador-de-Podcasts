4f97b2d6c096d904497bb4763abb05300a764b2c
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-02-25 10:13:44 -0300

    Criando rotas

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,5 +1,6 @@
 import {IncomingMessage,request,ServerResponse} from "http";
 import {serviceListEpisode} from '../services/list-episodes-service';
+import { serviceFilterEpisode } from "../services/filter-episodies-service";
 
 export const getListEpisodes = async (req : IncomingMessage, res: ServerResponse) => {
 
@@ -8,4 +9,13 @@ export const getListEpisodes = async (req : IncomingMessage, res: ServerResponse
     res.writeHead(200,{'content-type': "application/json"});
     res.end(JSON.stringify(content));
     
+};
+
+export const getFilterEpisodes = async(
+    req: IncomingMessage,
+    res: ServerResponse
+) => {
+    const content = await serviceFilterEpisode("flow");
+    res.writeHead(200, {"content-type":"application/json"});
+    res.end(JSON.stringify(content));
 };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import * as http from "http";
-import {getListEpisodes} from './controllers/podscasts-controller'
+import {getFilterEpisodes, getListEpisodes} from './controllers/podscasts-controller'
 
 const server = http.createServer(
     async (req: http.IncomingMessage, res: http.ServerResponse) => {
-        if(req.method === "GET") {
-          await getListEpisodes(req,res);
-        }
+
+      // Listar podcasts
+      if(req.method === "GET" && req.url === "/api/list") {
+        await getListEpisodes(req,res);
+      }
+
+      if(req.method === "GET" && req.url === "/api/episode"){
+        await getFilterEpisodes(req,res);
+      }
 }
 );
 
